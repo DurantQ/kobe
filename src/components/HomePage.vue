a7b0eb88991fe5cd5ed99cc98c741ee5c948c24f
unknown <1011542415@qq.com>
2024-09-26 21:53:15 +0800

    主页面创建

---
+++ src/components/HomePage.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue';
+import HheaderList from "../view/home/HheaderList.vue";
+import HimgSwiper from "../view/home/HimgSwiper.vue";
+import HmiddleList from "../view/home/HmiddleList.vue";
+import HmusicList from "../view/home/HmusicList.vue";
+import HyourSelect from "../view/home/HyourSelect.vue";
+const active = ref(0);
+</script>
+
+<template>
+        <HheaderList></HheaderList>
+        <HimgSwiper></HimgSwiper>
+        <HmiddleList></HmiddleList>
+        <HmusicList></HmusicList>
+        <HyourSelect></HyourSelect>
+        <div style="width: 100%; height: 7.5vh;z-index: 99;">
+            <van-tabbar v-model="active" active-color="#eb4755">
+                <van-tabbar-item replace to="/" icon="music-o">发现</van-tabbar-item>
+                <van-tabbar-item replace to="/BlogsPage" icon="bulb-o">播客</van-tabbar-item>
+                <van-tabbar-item replace to="/MyPage" icon="service-o">我的</van-tabbar-item>
+                <van-tabbar-item replace to="/AttentionPage" icon="friends-o">关注</van-tabbar-item>
+                <van-tabbar-item replace to="/CommunityPage" icon="chat-o">社区</van-tabbar-item>
+            </van-tabbar>
+        </div>
+</template>
+
+<style scoped>
+</style>
