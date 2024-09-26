a618c2f18725f46d5293e8ab481beed5feff5e8b
unknown <1011542415@qq.com>
2024-09-26 21:50:37 +0800

    全局注册

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import router from './router/index';
+import { Swipe, SwipeItem, Button, Icon, Field, CellGroup, SwipeCell, Slider, Tabbar, TabbarItem } from 'vant';
+import './elementindex.css';
+import 'vant/lib/index.css';
+import './style.css';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(Swipe);
+app.use(SwipeItem);
+app.use(Button);
+app.use(Icon);
+app.use(Field);
+app.use(CellGroup);
+app.use(SwipeCell);
+app.use(Slider);
+app.use(Tabbar);
+app.use(TabbarItem);
+app.use(router);
+app.mount('#app');
