4286287377271ff9d3d5acf91ddaca6d447ce83a
unknown <1011542415@qq.com>
2024-09-26 21:56:17 +0800

    viewhome页面的创建

---
+++ src/view/home/HheaderList.vue
@@ -0,0 +1,41 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <header>
+            <van-icon name="wap-nav" size="6vw" badge="99+" />
+            <div class="search">
+                <van-icon name="search" size="6vw" />
+                <input type="text" placeholder="请输入你想查找的歌曲">
+                <van-icon name="scan" size="6vw" />
+            </div>
+            <van-icon name="service-o" size="6vw" />
+        </header>
+    </div>
+</template>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 100vw;
+    height: 9vh;
+}
+
+input {
+    border: 0;
+    outline: none;
+    background-color: transparent;
+}
+
+header .search {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 72vw;
+    border-radius: 5vw;
+    border: 2px solid #d9caf6;
+}
+</style>
